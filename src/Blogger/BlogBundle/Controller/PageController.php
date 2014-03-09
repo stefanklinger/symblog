51dc00a90727bb6d2e433ac7ada36b96acae667c
Stefan Klinger <stefanklinger@yahoo.com>
2014-03-09 20:01:58 +0100

    contact with email

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -28,7 +28,15 @@ class PageController extends Controller {
             $form->bind($this->getRequest());
 
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
+                $message = \Swift_Message::newInstance()
+                        ->setSubject('Contact enquiry from symblog')
+                        ->setFrom('enquiries@symblog.co.uk')
+                        ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                        ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
                 return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));

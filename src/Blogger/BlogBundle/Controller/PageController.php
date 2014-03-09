20dc7913f8f4116355dd55c15c28a767b786d97b
Stefan Klinger <stefanklinger@yahoo.com>
2014-03-09 16:16:26 +0100

    contact page

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -1,19 +1,23 @@
 <?php
+
 // src/Blogger/BlogBundle/Controller/PageController.php
 
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PageController extends Controller
-{
-    public function indexAction()
-    {
+class PageController extends Controller {
+
+    public function indexAction() {
         return $this->render('BloggerBlogBundle:Page:index.html.twig');
     }
-    
-    public function aboutAction()
-    {
+
+    public function aboutAction() {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
+
+    public function contactAction() {
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+    }
+
 }
